718c7d67a45eed1b2d097337bf0b22211da24292
Olle Lundberg <geek@nerd.sh>
2016-04-19 20:00:22 +0200

    Correct module name for autoload.

--- lib/pristine.rb
+++ lib/pristine.rb
@@ -1,6 +1,6 @@
 require 'pristine/config'
 require 'pristine/daemon'
 module Pristine
-  autoload :Daemon, 'pristine/config'
+  autoload :Config, 'pristine/config'
   autoload :Daemon, 'pristine/daemon'
 end
